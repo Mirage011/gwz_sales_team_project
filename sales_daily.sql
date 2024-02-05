cf6fcabbd4e6d890e799c6b143180059cb90fa0a
A R <mrakaraza@gmail.com>
2024-02-05 16:39:58 +0100

    sales data added

--- sales_daily.sql
+++ sales_daily.sql
@@ -6,3 +6,24 @@ date_date
 ,sum(purchase_cost)
 FROM sales_daily
 grouo by category_1 , date_date
+SALES_DAILY
+
+
+SELECT
+  day,
+  sales,
+  SUM(sales) OVER (ORDER BY day) AS cumulative_sales
+FROM
+  sales
+ORDER BY
+  day;
+
+
+SELECT
+  day,
+  sales / 1000 AS daily_sales_in_k,
+  SUM(sales) OVER (ORDER BY day) / 1000 AS cumulative_sales_in_k
+FROM
+  sales
+ORDER BY
+  day;
