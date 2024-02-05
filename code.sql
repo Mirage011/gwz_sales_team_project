c43b5b8bf918aad1dad051cbf85f2888df4ab606
Mirage Frida <155905634+Mirage011@users.noreply.github.com>
2024-02-05 15:16:27 +0100

    review turnover - purchase_cost please

--- code.sql
+++ code.sql
@@ -5,4 +5,5 @@ date_date
 ,round(SUM(turnover) ,0) AS turnover
 ,round(SUM(turnover - purchase_cost) ,0) AS margin
 FROM banded-scion-411310.course17.gwz_sales_17
-group by category_1 , date_date
+group by category_1 
+, date_date
